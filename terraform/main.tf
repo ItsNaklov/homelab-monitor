8e1d2c8b42a5d04e97f15f4c9d6b7bc9cf2b9cad
ItsNaklov <outsider90@gmail.com>
2026-05-04 10:09:14 +0000

    feat: add terraform and github actions CI/CD pipeline

---
+++ terraform/main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+resource "kubernetes_namespace" "monitoring" {
+  metadata {
+    name = "monitoring"
+  }
+}
+
+resource "helm_release" "kube_prometheus_stack" {
+  name       = "kube-prometheus-stack"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "kube-prometheus-stack"
+  namespace  = kubernetes_namespace.monitoring.metadata[0].name
+
+  values = [file("${path.module}/../k3s/prometheus-values.yml")]
+
+  lifecycle {
+    ignore_changes = all
+  }
+}
